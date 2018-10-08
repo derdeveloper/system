a24f564459bea85b58d1251e2b9be4abffc439f3
luca145783 <email>
2018-10-08 17:46:30 +0200

    System-Plugin erstellt

---
+++ System/src/com/derdeveloper/system/Main.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) derdeveloper
+ * Die hier genutzten Methoden und Klassen sind Implementierungen aus der Craftbukkit API
+ * Der hier genutzte Code steht zur freien Verfügung
+ */
+
+package com.derdeveloper.system;
+
+/**
+  * 
+  * @author derdeveloper
+  * Erstellt am 08.10.2018 um 17:43:43
+  **/
+
+public class Main {
+
+}
